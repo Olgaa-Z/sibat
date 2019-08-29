0c79ae837487afdd886c78e9f9353ac741359c28
zelviaolgamaharani <zelviaolgamahaani98@gmail.com>
2019-08-28 21:41:11 -0700

    Initial commit

---
+++ app/src/main/java/com/hebat/sibat/sibat/ui/ui/TesActivity.kt
@@ -0,0 +1,12 @@
+package com.hebat.sibat.sibat.ui.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.hebat.sibat.sibat.R.layout.activity_tes)
+    }
+}
